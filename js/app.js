67397ca1cfbb2da46b805272d06efa0287e3134c
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2019-09-25 18:25:39 +0200

    base

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Variables
+const cart = document.getElementById('carrito');
+const products = document.getElementById('lista-cursos');
+const productsList = document.querySelector('#lista-carrito tbody');
+const cartEmptyBtn = document.getElementById('vaciar-carrito');
+
+
+// Listeners
+loadEventListeners();
+
+function loadEventListeners(){
+  
+}
+
+
+// Functions
